de886e32793c64db255a8deec489c6e2da44b181
Sidhdhrajsinh <sidhdhrajsinhdodiya007@gmail.com>
2025-07-26 12:40:45 +0000

    type conversion

---
+++ .devcontainer/01_basics/03_typeConversion.js
@@ -0,0 +1,11 @@
+// let score = true
+
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score); //note capital N
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); // NaN is NotANumber
+
+//------------operations-----------
+
