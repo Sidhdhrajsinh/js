f1854153338c25d5253488369a61caa192be426d
Sidhdhrajsinh <sidhdhrajsinhdodiya007@gmail.com>
2025-07-26 04:59:41 +0000

    variables and datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accId = 12323
+let accEmain = "sidh@gmail.com"
+var accPassword = "12345"
+accCity = "Masoorie"
+
+// accId = 2  // not allowed
+
+accEmain = "new@gmail.com"
+accPassword = "11111"
+accCity = "PA"
+
+console.log(accId);
+
+console.table([accId, accEmain, accPassword, accCity])
