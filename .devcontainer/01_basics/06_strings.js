81cc0ee1241209ee673a95accb446d75510f38aa
Sidhdhrajsinh <sidhdhrajsinhdodiya007@gmail.com>
2025-07-27 05:59:50 +0000

    strings

---
+++ .devcontainer/01_basics/06_strings.js
@@ -0,0 +1,35 @@
+// const name="sid"
+// const count = 50
+
+// console.log(name + count + true);
+
+// console.log(`Hello my name is ${name} and my balance is ${count} billions`);
+
+
+const newname = new String("sidhdhraj-sinh");
+// console.log(newname);
+// console.log(newname.__proto__);
+
+
+// console.log(newname.length);
+// console.log(newname.toUpperCase()); //won't change original
+// console.log(newname.charAt(1));
+// console.log(newname.indexOf('j'));
+
+
+// const fname = newname.substring(0,4);
+// console.log(fname);
+
+// ////const another = newname.slice(-1, 4);    //-ve not working for some reason
+// const another = newname.slice(0, 4);
+// console.log(another);
+
+// const trim = newname.trim();
+// console.log(trim);
+
+
+const url = "https://lol.com/lol%20lolismyname"
+console.log(url.replace("%20", '-'));
+
+console.log(newname.split('-'));
+
