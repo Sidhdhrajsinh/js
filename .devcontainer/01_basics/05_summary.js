61d125797311324623345a923626036d2bcc51e3
Sidhdhrajsinh <sidhdhrajsinhdodiya007@gmail.com>
2025-07-27 04:46:07 +0000

    dataTypeSummary

---
+++ .devcontainer/01_basics/05_summary.js
@@ -0,0 +1,17 @@
+// const name =["sidhdhrajsinh", "dodiya", "bapu"];
+
+// let obj = {
+//     namee: "sidhdh",
+//     age: 19
+// }
+
+// console.log(name);
+
+// console.log(obj);
+
+
+const myFunction = function(){
+    console.log("hello word!");
+}
+console.log(typeof(myFunction));
+myFunction(); //prints 
