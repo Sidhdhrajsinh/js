2ea01d575942d6524e6e8a9e08133f926d179cf7
Sidhdhrajsinh <sidhdhrajsinhdodiya007@gmail.com>
2025-07-27 12:30:07 +0000

    numbers_and_Math

---
+++ .devcontainer/01_basics/06_nums_maths.js
@@ -0,0 +1,51 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100.422442);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // . pachi ketla show karva ae
+
+// const otherNumber = 128.6353
+
+// console.log(otherNumber.toPrecision(3)); //next to int ne asssume kari lese
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString()); // adds comma for easy readability [US format]
+// console.log(hundreds.toLocaleString('en-IN')); // india format
+
+// console.log(typeof (hundreds.toLocaleString()));
+// console.log(typeof (hundreds.toLocaleString('en-IN')));
+
+// console.log(Number.MAX_VALUE);
+
+
+
+//=================== Maths =======================
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // -ve to +ve but not vice versa
+// console.log(Math.abs(4));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(3.4)); //ceiling
+// console.log(Math.floor(3.9)); //floor
+// console.log(Math.min( 1, 2, 3, 4, 5)); // min value, same max available
+// console.log(Math.random());
+// console.log(Math.round(Math.random()*10%6)); // invented dice generator
+
+
+//--------------------IMP-----------
+// const min = 10;
+// const max = 60;
+
+// console.log(Math.floor(Math.random() * (max- min + 1)) + min)
+
+
+
+
+
+console.log();
+console.log();
+console.log();
+
