ea540f80279b4014b644624504b47c8bd6cda9ab
Sidhdhrajsinh <sidhdhrajsinhdodiya007@gmail.com>
2025-07-27 04:57:41 +0000

    stack heap

--- .devcontainer/01_basics/05_summary.js
+++ .devcontainer/01_basics/05_summary.js
@@ -10,8 +10,28 @@
 // console.log(obj);
 
 
-const myFunction = function(){
-    console.log("hello word!");
+// const myFunction = function(){
+//     console.log("hello word!");
+// }
+// console.log(typeof(myFunction));
+// myFunction(); //prints 
+
+
+
+
+// -------------------------memory---------------------
+
+//----stack - primitive - copy
+//----heap - non primitive - reference
+
+let userOne = {
+    email: "oldmail.com",
+    name: "old"
 }
-console.log(typeof(myFunction));
-myFunction(); //prints 
+
+let userTwo = userOne
+
+userTwo.email = "newmail.com"   //will also change them rigional mail
+
+console.log(userOne.email);
+console.log(userTwo.email);  
