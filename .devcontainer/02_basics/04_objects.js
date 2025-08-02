57f9f29b255ba213e9485caac8fb6f2f8bb01a20
Sidhdhrajsinh <sidhdhrajsinhdodiya007@gmail.com>
2025-08-02 05:33:10 +0000

    Objects2

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,51 @@
+const tinderUser = new Object();
+// const tinderUser = {}
+// console.log(tinderUser);
+
+tinderUser.id = "123abc"
+tinderUser.name = "sidhdhrajsinh"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+
+const regularUser = {
+    email: "sidhdmail.com",
+    fullname: {
+        firstname: "sidh",
+        lastname: "dodiya"
+    },
+    dob: "123"
+}
+
+
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.lastname);
+// console.log(regularUser.firstname);
+
+
+
+const obj1 = {
+    1: 'a',
+    2: 'b'
+}
+const obj2 = {
+    3: 'a',
+    4: 'b'
+}
+
+// const obj3 = {obj1, obj2} // //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+// const obj3 = Object.assign(obj1, obj2) //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+//const obj3 = Object.assign({},obj1, obj2, obj4, obj5) //for >2
+// console.log(obj3); 
+
+const obj3 = {...obj1, ...obj2} //spread- easy
+console.log(obj3); 
+
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
